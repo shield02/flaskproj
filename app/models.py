0e4285ac20c68975580d14a8e96d35abd8ebfd89
Otuekong Etim <myshield02@gmail.com>
2024-01-11 11:35:50 +0100

    implemented password hashing

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
@@ -16,6 +17,12 @@ class User(db.Model):
 
     def __repr__(self) -> str:
         return f'<User {self.username}>'
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
