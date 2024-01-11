8cafaa23e47b4733e7a035d05b9d46e226c407d8
Otuekong Etim <myshield02@gmail.com>
2024-01-11 11:25:30 +0100

    modified the user model to reference post and create a new post model

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timezone
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as orm
+from app import db
+
+class User(db.Model):
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    username: orm.Mapped[str] = orm.mapped_column(sa.String(64), index=True,
+                                                  unique=True)
+    email: orm.Mapped[str] = orm.mapped_column(sa.String(120), index=True,
+                                               unique=True)
+    password_hash: orm.Mapped[Optional[str]] = orm.mapped_column(sa.String(256))
+    posts: orm.WriteOnlyMapped['Post'] = orm.relationship(
+        back_populates='author')
+
+    def __repr__(self) -> str:
+        return f'<User {self.username}>'
+
+
+class Post(db.Model):
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    body: orm.Mapped[int] = orm.mapped_column(sa.String(140))
+    timestamp: orm.Mapped[datetime] = orm.mapped_column(index=True,
+                                                        default=lambda:
+                                                            datetime.now(
+                                                                timezone.utc))
+    user_id: orm.Mapped[int] = orm.mapped_column(sa.ForeignKey(User.id),
+                                                 index=True)
+    author: orm.Mapped[User] = orm.relationship(back_populates='posts')
+
+    def __repr__(self) -> str:
+        return f'<Post {self.body}>'
