ebb8d0a4e7b92ba64fb74f05521dbdf06c7d0f64
Otuekong Etim <myshield02@gmail.com>
2024-01-15 15:36:59 +0100

    route to edit user profile

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,7 @@ from app import app, db
 from app.models import User
 from app.auth.login import LoginForm
 from app.auth.register import RegistrationForm
+from app.forms import EditProfileForm
 
 @app.route('/')
 @app.route('/index')
@@ -84,3 +85,19 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.now(timezone.utc)
         db.session.commit()
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form=form)
