f904d5d77cc3c8792bc9a6e53dec8ba1670d3f0e
Otuekong Etim <myshield02@gmail.com>
2024-04-18 05:02:29 +0100

    three test written

---
+++ tests.py
@@ -0,0 +1,87 @@
+import os
+os.environ['DATABASE_URL'] = 'sqlite://'
+
+from datetime import datetime, timezone, timedelta
+import unittest
+from hashlib import md5
+from app import app, db
+from app.models import User, Post
+
+
+class UserModelClass(unittest.TestCase):
+    def setUp(self):
+        self.app_context = app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_password_hashing(self):
+        u = User(username='susan', email='susan@example.com')
+        u.set_password('pass')
+        self.assertFalse(u.check_password('past'))
+        self.assertTrue(u.check_password('pass'))
+
+    def test_avatar(self):
+        u = User(username='john', email='john@exmple.com')
+        digest = '5dc7e712608a696c29f7aaa2612a16e9'
+        self.assertEqual(u.avatar(128), (f'https://www.gravatar.com/avatar/{digest}?d=identicon&s=128'))
+
+    def test_follow(self):
+        u1 = User(username='john', email='john@example.com')
+        u2 = User(username='susan', email='susan@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        following = db.session.scalars(u1.following.select()).all()
+        followers = db.session.scalars(u1.followers.select()).all()
+        self.assertEqual(following, [])
+        self.assertEqual(followers, [])
+
+        u1.follow(u2)
+        db.session.commit()
+        self.assertTrue(u1.is_following(u2))
+        self.assertEqual(u1.following_count(), 1)
+        self.assertEqual(u2.followers_count(), 1)
+        u1_following = db.session.scalars(u1.following.select()).all()
+        u2_followers = db.session.scalars(u2.followers.select()).all()
+        self.assertEqual(u1_following[0].username, 'susan')
+        self.assertEqual(u2_followers[0].username, 'john')
+
+        u1.unfollow(u2)
+        db.session.commit()
+        self.assertFalse(u1.is_following(u2))
+        self.assertEqual(u1.following_count(), 0)
+        self.assertEqual(u2.followers_count(), 0)
+
+    def test_follow_post(self):
+        # create 4 users
+        u1 = User(username='john', email='john@example.com')
+        u2 = User(username='susan', email='susan@example.com')
+        u3 = User(username='mary', email='mary@example.com')
+        u4 = User(username='david', email='david@example.com')
+        db.session.add_all([u1, u2, u3, u4])
+
+        # create 4 post
+        now = datetime.now(timezone.utc)
+        p1 = Post(body="post from john", author=u1, timestamp=now + timedelta(seconds=1))
+        p2 = Post(body="post from susan", author=u2, timestamp=now + timedelta(seconds=4))
+        p3 = Post(body="post from mary", author=u3, timestamp=now + timedelta(seconds=3))
+        p4 = Post(body="post from david", author=u4, timestamp=now + timedelta(seconds=2))
+        db.session.add_all([p1, p2, p3, p4])
+        db.session.commit()
+
+        # Set up the followers
+        u1.follow(u2)  # john follows susan
+        u1.follow(u4)  # john follows david
+        u2.follow(u3)  # susan follows mary
+        u3.follow(u4)  # mary follows david
+        db.session.commit()
+
+        # Check the following post of each user
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
