fc46e7e994e2f40945fe49df90ec18aafbda63a0
Otuekong Etim <myshield02@gmail.com>
2024-04-27 03:42:52 +0100

    querying database for real post

--- app/routes.py
+++ app/routes.py
@@ -73,10 +73,8 @@ def register():
 @login_required
 def user(username):
     user = db.first_or_404(sa.select(User).where(User.username == username))
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    query = sa.select(Post).order_by(Post.timestamp.desc())
+    posts = db.session.scalars(query).all()
     return render_template('user.html', user=user, posts=posts)
 
 @app.before_request
