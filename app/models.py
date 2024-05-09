d30ac07b62f35e4c8db140fc9875e2f96c91eb00
Otuekong Etim <myshield02@gmail.com>
2024-05-09 07:03:22 +0100

    implement reset password token and a static method to verify password token

--- app/models.py
+++ app/models.py
@@ -4,9 +4,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from typing import Optional
 from flask_login import UserMixin
 from hashlib import md5
+from time import time
+import jwt
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
-from app import db, login
+from app import app, db, login
 
 followers = sa.Table(
     'followers',
@@ -46,6 +48,20 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return db.session.get(User, id)
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
