f5e42fe84740271a60fc704251c21da5184048ac
Otuekong Etim <myshield02@gmail.com>
2024-05-17 07:19:35 +0100

    added a way for the lang of a post to be detected on submission

--- app/routes.py
+++ app/routes.py
@@ -10,6 +10,7 @@ from app.auth.login import LoginForm
 from app.auth.register import RegistrationForm
 from app.forms import EditProfileForm, EmptyForm, PostForm, ResetPasswordRequestForm
 from app.email import send_password_reset_email
+from langdetect import detect, LangDetectException
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -17,7 +18,12 @@ from app.email import send_password_reset_email
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        try:
+            language = detect(form.post.data)
+        except LangDetectException:
+            language = ''
+        post = Post(body=form.post.data, author=current_user,
+                    language=language)
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
