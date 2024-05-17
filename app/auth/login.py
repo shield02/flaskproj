0efb61fb0090181bfa680a47f8e2260169d51fba
Otuekong Etim <myshield02@gmail.com>
2024-05-17 06:41:31 +0100

    added babel support for language translation

--- app/auth/login.py
+++ app/auth/login.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 from flask_wtf import FlaskForm
+from flask_babel import lazy_gettext as _l
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField(_l('Username'), validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
